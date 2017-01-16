f805dee9763e9e5ca4d763f294d27c2614f00493
Maxim Topciu <maximtop@gmail.com>
2017-01-16 15:37:39 +0300

    adds name asking

--- src/brain-even.js
+++ src/brain-even.js
@@ -1,4 +1,7 @@
-// @flow
+import readlineSync from 'readline-sync';
+
 export default () => {
   console.log ('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}`);
 }
